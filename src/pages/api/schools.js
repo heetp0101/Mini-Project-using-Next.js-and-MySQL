28498f992a977f73412e389c02f40747469c713e
Patel Heet <heetp0101@gmail.com>
2025-09-01 19:05:24 +0530

    schools.js file updated

--- src/pages/api/schools.js
+++ src/pages/api/schools.js
@@ -1,4 +1,5 @@
-import {query} from "../../../lib/db";
+// import {query} from "../../../lib/db";
+import { query } from "@lib/db";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
